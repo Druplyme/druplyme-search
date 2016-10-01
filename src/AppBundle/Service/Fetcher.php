df3243992a54bdb426122f1d2449787792b754bc
Alan Bondarchuk <imacoda@gmail.com>
2016-10-02 01:23:37 +0500

    Provide Encoder as service

--- src/AppBundle/Service/Fetcher.php
+++ src/AppBundle/Service/Fetcher.php
@@ -10,21 +10,19 @@ class Fetcher
     /** @var XmlEncoder $encoder */
     private $encoder;
 
-    public function __construct()
+    const PROJECTS_URL = 'https://updates.drupal.org/release-history/project-list/all';
+
+    public function __construct($encoder, $rooNodeName)
     {
-        $this->encoder = new XmlEncoder('projects');
+        $this->encoder = $encoder;
+        $this->encoder->setRootNodeName($rooNodeName);
     }
 
     public function fetch()
     {
-        $data = file_get_contents('https://updates.drupal.org/release-history/project-list/all');
+        $data = file_get_contents(self::PROJECTS_URL);
         $decoded = $this->encoder->decode($data, 'xml');
-
-//        $serializer = new Serializer(
-//          [new GetSetMethodNormalizer(), new ArrayDenormalizer()],
-//          [new XmlEncoder('projects')]
-//        );
-//        $decoded = $serializer->deserialize($data, 'AppBundle\Entity\Project[]', 'xml');
+//        $decoded = $this->get('serializer')->deserialize($data, 'AppBundle\Entity\Project[]', 'xml');
 
         $projects = array_map(function ($data) {
             return new Project($data);
