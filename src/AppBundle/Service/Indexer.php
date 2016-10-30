2150c73fa0784fc0baba7b921916a579d5b59929
Alan Bondarchuk <imacoda@gmail.com>
2016-10-30 05:46:09 +0500

    Use chunks for indexing

--- src/AppBundle/Service/Indexer.php
+++ src/AppBundle/Service/Indexer.php
@@ -3,26 +3,32 @@
 namespace AppBundle\Service;
 
 use AppBundle\Repository\ProjectRepository;
+use Symfony\Component\DependencyInjection\Container;
 
 class Indexer
 {
+
     /** @var ProjectRepository $repository */
     private $repository;
 
     /** @var Fetcher $fetcher */
     private $fetcher;
 
-    public function __construct(ProjectRepository $repository, Fetcher $fetcher)
+    public function __construct(ProjectRepository $repository, Fetcher $fetcher, Container $container)
     {
         $this->fetcher = $fetcher;
         $this->repository = $repository;
+        $this->container = $container;
     }
 
     public function index($prefetch = false)
     {
         $projects = $prefetch ? $this->fetcher->fetch() : $this->repository->findAll();
+        $solr = $this->container->get('solr.client');
 
-        //        $container->get('solr.client')
-//                  ->synchronizeIndex($projects);
+        foreach (array_chunk($projects, 300) as $entities) {
+            $solr->synchronizeIndex($entities);
+        }
+
     }
 }
